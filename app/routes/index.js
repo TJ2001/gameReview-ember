9862be34863412e783234a2a404f0d4652fa6e6e
TJ2001 <jung.m.tim@gmail.com>
2016-08-26 13:03:47 -0700

    destroy stupid bug

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('game');
+  model(params) {
+    return this.store.findAll('game', params.rental_id);
   },
 
   actions: {
     saveGame2(params) {
-      console.log(params);
       var newGame = this.store.createRecord('game', params);
       newGame.save();
       this.transitionTo('index');
