ee8fa01d61c0b95fabc22b07713d7f9d63e85e3e
TJ2001 <jung.m.tim@gmail.com>
2016-08-26 15:18:16 -0700

    add star-rating helper

--- app/helpers/star-rating.js
+++ app/helpers/star-rating.js
@@ -1,7 +1,18 @@
 import Ember from 'ember';
 
-export function starRating(params/*, hash*/) {
-  return params;
-}
+export function starRating(params) {
+  var reviewRating = params[0].get('rating');
 
+  if (reviewRating >= 5){
+      return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span>');
+  } else if (reviewRating >= 4){
+      return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star">');
+  } else if (reviewRating >= 3){
+      return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star">');
+  } else if (reviewRating <= 2){
+      return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star">');
+  } else if (reviewRating <= 2){
+      return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span>');
+  }
+}
 export default Ember.Helper.helper(starRating);
